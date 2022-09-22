f8855365af8fb9a8101706a31b5024b13a6a68af
sreesankar362 <sreesankar362@gmail.com>
2022-09-22 23:36:53 +0530

    test image

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 from.views import HomeView
 
 urlpatterns = [
     path('',HomeView.as_view(),name="home")
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
