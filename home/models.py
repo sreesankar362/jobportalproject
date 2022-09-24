9e3ed870778a408206e422153b875e40ed954762
greeshma <greeshmajayaghosh@gmail.com>
2022-09-24 08:52:57 +0530

    test commit greeshma

--- home/models.py
+++ home/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.core.validators import FileExtensionValidator
 # Create your models here.
+<<<<<<< Updated upstream
 class DemoModel(models.Model):
   print("hello")
 
@@ -16,3 +18,27 @@ class ModelDemosecond(models.Model):
 class ModelDemoThird(models.Model):
     print("hi")
     print("seconf hi")
+=======
+
+
+class ApplicantUser(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,unique=True)
+    email = models.EmailField(unique=True,blank=False,error_messages={"unique":"This email already exists. Already a member?"})
+    is_mail_verified = models.BooleanField(default=False)
+    dob = models.DateField(null=True)
+    mobile = models.CharField(max_length=20,null=True)
+    is_phone_verified = models.BooleanField(default=False)
+    profile_pic = models.ImageField(upload_to="media",null=True,validators=[FileExtensionValidator(allowed_extensions=['jpg','png','jpeg'])])
+
+    options = (
+        ("male", "Male"),
+        ("Female", "Female"),
+        ("Others", "Others")
+    )
+    gender = models.CharField(max_length=10,choices=options)
+    bio = models.TextField(max_length=500,blank=True)
+    location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user.username
+>>>>>>> Stashed changes
