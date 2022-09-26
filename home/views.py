8eab2c770e74e376f32c78f8a0245cb5fdec2951
sreesankar362 <sreesankar362@gmail.com>
2022-09-27 00:38:52 +0530

    jobs link added job listing template & commom html file sorted

--- home/views.py
+++ home/views.py
@@ -5,3 +5,8 @@ from django.views.generic import View
 class HomeView(View):
     def get(self,request):
         return render(request,"home.html")
+
+
+class JobListingView(View):
+    def get(self,request):
+        return render(request,"job_listing.html")
