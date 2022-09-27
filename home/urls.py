276ee4a80b8c1b6d6ec16d0fadcdbd73d17df6c2
bibinkunjumon <bibinkunjumon2020@gmail.com>
2022-09-27 20:11:28 +0300

    First Commit

--- home/urls.py
+++ home/urls.py
@@ -3,8 +3,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from.views import HomeView
 
+
 urlpatterns = [
-    path('',HomeView.as_view(),name="home")
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('',HomeView.as_view(),name="home"),
+    path('user/',include("user.urls"))
+
+    ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
 
