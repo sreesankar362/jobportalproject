40244f25b8dbe13203b6688b850629122b760b34
tinumaria <tinumariad@gmail.com>
2022-10-03 13:56:12 +0530

    stage1

--- companyaccount/forms.py
+++ companyaccount/forms.py
@@ -1,12 +1,17 @@
-
 from django import forms
 from companyaccount.models import CompanyProfile
 from accounts.models import User
-
+from user.forms import RegistrationForm
 
 
 class CompanyRegistrationForm(forms.ModelForm):
     class Meta:
         model = CompanyProfile
         fields = ['company_name']
+        
+        widgets = {
+            "company_name": forms.TextInput(attrs={"class":"form-control"})
+        }
+
+
 
--- user/forms.py
+++ user/forms.py
@@ -23,3 +23,17 @@ class RegistrationForm(forms.ModelForm):
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'phone_number', 'password']
 
+        widgets = {
+            "username": forms.TextInput(attrs={"class":"form-control"}),
+            "email": forms.EmailInput(attrs={"class": "form-control"}),
+            "first_name": forms.TextInput(attrs={"class":"form-control"}),
+            "last_name": forms.TextInput(attrs={"class":"form-control"}),
+            "phone_number": forms.TextInput(attrs={"class":"form-control"}),
+        }
+
+    def clean(self):
+        cleaned_data = super(RegistrationForm,self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+        if password != confirm_password:
+            raise forms.ValidationError("passwords does not match")
