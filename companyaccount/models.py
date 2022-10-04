7bb8d9eb1656021c741df4557142daffc1b36b93
sreesankar362 <sreesankar362@gmail.com>
2022-10-05 02:30:36 +0530

    added company dashboard,pw change,profile update

--- companyaccount/models.py
+++ companyaccount/models.py
@@ -3,8 +3,6 @@ from accounts.models import User
 from django.core.validators import FileExtensionValidator
 
 
-# Create your models here.
-
 class SocialProfile(models.Model):
     website = models.URLField(default="", null=True)
     fb = models.URLField(default="", null=True)
@@ -18,7 +16,7 @@ class CompanyProfile(models.Model):
     company_logo = models.ImageField(
         upload_to="company_images",
         validators=[FileExtensionValidator(allowed_extensions=["jpg", "png", "jpeg"])],
-        null=True
+        null=True,blank=True
     )
     company_description = models.CharField(max_length=500, null=True, blank=True)
     location = models.CharField(max_length=100, null=True, blank=True)
@@ -34,9 +32,10 @@ class CompanyProfile(models.Model):
     founded = models.PositiveIntegerField(null=True, blank=True)
     company_address = models.CharField(max_length=250, null=True, blank=True)
     country_code = models.PositiveIntegerField(null=True, blank=True)
-    social_profile = models.ForeignKey(SocialProfile, on_delete=models.CASCADE, null=True, blank=True)
+    # social_profile = models.ForeignKey(SocialProfile, on_delete=models.CASCADE, null=True, blank=True)
 
     is_approved = models.BooleanField(default=False)
+    is_mail_verified = models.BooleanField(default=False)
 
     def __str__(self):
         return self.company_name
