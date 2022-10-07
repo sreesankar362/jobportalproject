2664cdcf0ffdcd68f7a3a97e98537250e7ec1b5f
NikhilChacko <nikhilchacko1470@gmail.com>
2022-10-07 16:21:02 +0530

    commit nikhil

--- candidate/admin.py
+++ candidate/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(CandidateProfile)
+
+admin.site.register(LatEducation)
+
+admin.site.register(Experience)
+
+admin.site.register(Skill)
--- jobportalproject/settings.py
+++ jobportalproject/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'home',
     'user',
     'companyaccount',
-    'accounts'
+    'accounts',
     'candidate'
 ]
 
