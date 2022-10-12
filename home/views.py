6a7e5de07fd9e402f7c0415811be0df54451703b
greeshma <greeshmajayaghosh@gmail.com>
2022-10-12 23:11:00 +0530

    can profile

--- home/views.py
+++ home/views.py
@@ -25,9 +25,7 @@ class JobListingView(TemplateView):
         if request.user.is_authenticated:
             saved_job = SavedJobs.objects.all().filter(user=request.user)
             print(saved_job)
-            for obj in saved_job:
-               saved = obj.job.all()
-               print(saved)
+
 
         context = {
             "all_jobs": all_jobs,
