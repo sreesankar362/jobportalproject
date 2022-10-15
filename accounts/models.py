964a9278034e39ccd60201fd12d466696287808b
bibinkunjumon <bibinkunjumon2020@gmail.com>
2022-10-15 14:02:35 +0300

    oct 15

--- accounts/models.py
+++ accounts/models.py
@@ -82,3 +82,9 @@ class User(AbstractBaseUser):
         return user_role
 
 # settings.py AUTH_USER_MODEL = 'accounts.User'
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = "%s %s" % (self.first_name, self.last_name)
+        return full_name.strip()
