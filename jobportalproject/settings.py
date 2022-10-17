b97bc1f1d431b07acc20b116a3bf6327b506abd0
greeshma <greeshmajayaghosh@gmail.com>
2022-10-17 19:37:55 +0530

    merging

--- jobportalproject/settings.py
+++ jobportalproject/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "1zchubhbia=ym^ye1rr7!@rbv*yx%g4fezul(@mc5p=1zat"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['*']
