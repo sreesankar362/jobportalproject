20a32fb6f014ce40b84ab15f711df56ab7e1065a
sreesankar362 <sreesankar362@gmail.com>
2022-10-17 19:42:58 +0530

    msg

--- home/filters.py
+++ home/filters.py
@@ -2,17 +2,18 @@ from dataclasses import field
 import django_filters
 from .models import JobModel
 
+
 class JobListingFilter(django_filters.FilterSet):
       
     class Meta:
          
         model = JobModel
-        fields =  {
+        fields = {
             'position':   ['icontains'],  
             'job_type':  ['exact'], 
             'work_type': ['exact'], 
             'company__location': ['icontains'],
             'min_experience': ['lte'],
-            'max_salary' : ['gte'],
+            'max_salary': ['gte'],
             }
       
